724b5d87ae62cffe5aa50522ca73f87241a0a045
Michael Tigue <mtigue@web4uonline.com>
2013-08-08 14:11:35 -0400

    1

---
+++ _vti_cnf/inc-footer.js
@@ -0,0 +1,9 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|08 Aug 2013 15:52:07 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_author:SR|web4u\\mtigue
+vti_modifiedby:SR|web4u\\mtigue
+vti_timecreated:TR|08 Aug 2013 15:52:07 -0000
+vti_cacheddtm:TX|08 Aug 2013 15:52:07 -0000
+vti_filesize:IR|832
+vti_backlinkinfo:VX|23/main-working-smooth.html 23/main.html
---
+++ _vti_cnf/pushwoosh.js
@@ -0,0 +1,9 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|02 Jun 2013 21:32:29 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_author:SR|web4u\\mtigue
+vti_modifiedby:SR|web4u\\mtigue
+vti_timecreated:TR|02 Jun 2013 21:32:29 -0000
+vti_cacheddtm:TX|02 Jun 2013 21:32:29 -0000
+vti_filesize:IR|3450
+vti_backlinkinfo:VX|23/main-working-smooth.html 23/main.html
